9a4120b26bd02e4e11009ea973b4f2f92edf8335
David Lovíšek <davidlovisek@gmail.com>
2026-09-19 22:49:03 +0200

    Add task 2 exercise 3 solution

--- 02-tiny-basic/tasks/03-conditionals-and-vars.fs
+++ 02-tiny-basic/tasks/03-conditionals-and-vars.fs
@@ -28,7 +28,8 @@ type Command =
 
 type State = 
   { Program : list<int * Command> 
-    // TODO: Add variable context to the program state
+    // DONE: Add variable context to the program state
+    VariableContext : Map<string, Value>
   }
 
 // ----------------------------------------------------------------------------
@@ -36,36 +37,81 @@ type State =
 // ----------------------------------------------------------------------------
 
 let printValue value = 
-  // TODO: Add support for printing NumberValue and BoolValue
-  failwith "implemented in step 1"
+  // DONE: Take 'value' of type 'Value', pattern match on it and print it nicely.
+  match value with
+  | StringValue str -> printf "%s" str
+  // DONE: Add support for printing NumberValue and BoolValue
+  | NumberValue num -> printf "%d" num
+  | BoolValue bool -> printf "%b" bool
 
-let getLine state line = failwith "implemented in step 1"
-let addLine state (line, cmd) = failwith "implemented in step 2"
+let getLine state line = 
+  // DONE: Get a line with a given number from 'state.Program' (this can fail 
+  // if the line is not there.) You need this in the 'Goto' command case below.
+  let lineCommand = List.filter (fun (num, cmd) -> num = line) state.Program
+
+  match lineCommand with
+  | [(line, cmd)] -> (line, cmd)
+  | [] -> failwith "Line not found"
+  | _ -> failwith "Multiple lines with the same number"
+
+let addLine state (line, cmd) = 
+  // DONE: Add a given line to the program state. This should overwrite 
+  // a previous line (if there is one with the same number) and also ensure
+  // that state.Program is sorted by the line number.
+  // HINT: Use List.filter and List.sortBy. Use F# Interactive to test them!
+  let newProgram = 
+    state.Program
+    |> List.filter (fun (lnum, lcmd) -> lnum <> line)
+    |> List.append [line, cmd]
+    |> List.sortBy (fun (lnum, lcmd) -> lnum)
+      
+  { state with Program = newProgram }
 
 // ----------------------------------------------------------------------------
 // Evaluator
 // ----------------------------------------------------------------------------
 
-let rec evalExpression expr = 
-  // TODO: Add support for 'Function' and 'Variable'. For now, handle just the two
+let rec evalExpression state expr =
+  // DONE: Implement evaluation of expressions. The function should take 
+  // 'Expression' and return 'Value'. In this step, it is trivial :-)  
+  match expr with 
+  | Const c -> c
+
+  // DONE: Add support for 'Function' and 'Variable'. For now, handle just the two
   // functions we need, i.e. "-" (takes two numbers & returns a number) and "="
   // (takes two values and returns Boolean). Note that you can test if two
   // F# values are the same using '='. It works on values of type 'Value' too.
   //
   // HINT: You will need to pass the program state to 'evalExpression' 
   // in order to be able to handle variables!
-  failwith "implemented in step 1"
+  | Variable v ->
+    match state.VariableContext.TryFind v with 
+      | Some res -> res
+      | _ -> failwith ("unbound variable: " + v)
+  | Function (op, exprList) ->
+    let values = exprList |> List.map (fun expr -> evalExpression state expr)
+      
+    match op, values with
+    | "-", [NumberValue x; NumberValue y] -> NumberValue(x - y)
+    | "=", [x; y] -> BoolValue(x = y)
+    | _ -> failwith $"Unknown operator: {op}"
 
 let rec runCommand state (line, cmd) =
   match cmd with 
   | Run ->
       let first = List.head state.Program    
       runCommand state first
+  | Print(expr) -> 
+    // DONE: Evaluate the expression and print the resulting value here!
+    printValue (evalExpression state expr)
 
-  | Print(expr) -> failwith "implemented in step 1"
-  | Goto(line) -> failwith "implemented in step 1"
+    runNextLine state line
+  | Goto(line) -> 
+    // DONE: Find the right line of the program using 'getLine' and call 
+    // 'runCommand' recursively on the found line to evaluate it.
+    runCommand state (getLine state line)
   
-  // TODO: Implement assignment and conditional. Assignment should run the
+  // DONE: Implement assignment and conditional. Assignment should run the
   // next line after setting the variable value. 'If' is a bit trickier:
   // * 'L1: IF TRUE THEN GOTO <L2>' will continue evaluating on line 'L2'
   // * 'L1: IF FALSE THEN GOTO <L2>' will continue on line after 'L1'
@@ -73,41 +119,70 @@ let rec runCommand state (line, cmd) =
   //
   // HINT: If <e> evaluates to TRUE, you can call 'runCommand' recursively with
   // the command in the 'THEN' branch and the current line as the line number.
-  | Assign _ | If _ -> failwith "not implemented"
+  | Assign(var, expr) ->
+    let result = evalExpression state expr
+    let newVarCtx = 
+      state.VariableContext.Add(var, result)
 
-and runNextLine state line = failwith "implemented in step 1"
+    runNextLine { state with VariableContext = newVarCtx } line
+  | If (expr, currentCmd) ->
+    let exprResult = evalExpression state expr
+    
+    if exprResult = BoolValue(true) 
+    then runCommand state (line, currentCmd)
+    else runNextLine state line
+
+and runNextLine state line = 
+  // DONE: Find a program line with the number greater than 'line' and evalaute
+  // it using 'runCommand' (if found) or just return 'state' (if not found).
+  let nextLine = List.filter (fun (num, _) -> num > line) state.Program |> List.tryHead
+  
+  match nextLine with // if Option.isNone nextLine then state else runCommand state nextLine.Value
+  | Some next -> runCommand state next
+  | None -> state
 
 // ----------------------------------------------------------------------------
 // Interactive program editing
 // ----------------------------------------------------------------------------
 
-let runInput state (line, cmd) = failwith "implemented in step 2"
-let runInputs state cmds = failwith "implemented in step 2"
+let runInput state (line, cmd) = 
+  // DONE: Simulate what happens when the user enters a line of code in the 
+  // interactive terminal. If the 'line' number is 'Some ln', we want to 
+  // insert the line into the right location of the program (addLine); if it
+  // is 'None', then we want to run it immediately. To make sure that 
+  // 'runCommand' does not try to run anything afterwards, you can pass 
+  // 'System.Int32.MaxValue' as the line number to it (or you could use -1
+  // and handle that case specially in 'runNextLine')
+  match line with
+  | Some num -> addLine state (num, cmd)
+  | None -> runCommand state (-1, cmd)
+
+let runInputs state cmds = 
+  //DONE: Apply all the specified commands to the program state using 'runInput'.
+  //This is a one-liner if you use 'List.fold' which has the following type:
+  //  ('State -> 'T -> 'State) -> 'State -> list<'T> -> 'State
+  List.fold (fun state input -> runInput state input) state cmds
 
 // ----------------------------------------------------------------------------
 // Test cases
 // ----------------------------------------------------------------------------
 
-let empty = { Program = [] } // TODO: Add empty variables to the initial state!
-
-let helloOnce = 
-  [ Some 10, Print (Const (StringValue "HELLO WORLD\n")) 
-    Some 10, Print (Const (StringValue "HELLO NPRG077\n")) 
-    None, Run ]
-
-let helloInf = 
-  [ Some 20, Goto 10
-    Some 10, Print (Const (StringValue "HELLO WORLD\n")) 
-    Some 10, Print (Const (StringValue "HELLO NPRG077\n")) 
-    None, Run ]
+let empty =
+      { Program = []
+        VariableContext = Map.empty } // DONE: Add empty variables to the initial state!
 
 let testVariables = 
   [ Some 10, Assign("S", Const(StringValue "HELLO WORLD\n")) 
     Some 20, Assign("I", Const(NumberValue 1))
     Some 30, Assign("B", Function("=", [Variable("I"); Const(NumberValue 1)]))
     Some 40, Print(Variable "S") 
-    Some 50, Print(Variable "I") 
+    Some 50, Print(Variable "I")
+    Some 55, Print(Const(StringValue("\n"))) 
     Some 60, Print(Variable "B")
+    Some 65, Print(Const(StringValue("\n")))
+    Some 70, Print(Const(StringValue("\n")))
+    Some 75, Print(Const(StringValue("\n")))
+    Some 80, Print(Const(StringValue("\n")))
     None, Run ]
 
 // NOTE: Simpler test program without 'If" (just variables and '=' function) 
